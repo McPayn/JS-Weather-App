27dd0df4b51b99cabfad8d910a01639dffaccd5f
Tyler Brown <brown.tyler.w@gmail.com>
2023-08-16 10:52:16 -0700

    Changed API URL Includes daily minimum temperature now

--- weather.js
+++ weather.js
@@ -1,12 +1,9 @@
 import axios from "axios";
 
-// API link
-// May need changes to fit desired data
-// https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=temperature_2m,precipitation_probability,precipitation,weathercode,cloudcover,windspeed_10m&daily=weathercode,temperature_2m_max,sunrise,sunset,precipitation_sum&timezone=America%2FLos_Angeles
-
 // API call to retrieve desired weather data
+// Nothing more should need to be done here
 export function getWeather(lat, lon, timezone) {
-    return axios.get('https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=temperature_2m,precipitation_probability,precipitation,weathercode,cloudcover,windspeed_10m&temperature_unit=fahrenheit&daily=weathercode,temperature_2m_max,sunrise,sunset,precipitation_sum&timezone=America%2FLos_Angeles', {
+    return axios.get('https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=temperature_2m,precipitation_probability,precipitation,weathercode,cloudcover,windspeed_10m&temperature_unit=fahrenheit&daily=weathercode,temperature_2m_max,temperature_2m_min,sunrise,sunset,precipitation_sum&timezone=America%2FLos_Angeles', {
         params: {
             latitude: lat,
             longitude: lon,
