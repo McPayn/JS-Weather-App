b6c5866fa9d935a72a8a85da26f5de0c1fc6e3f3
Tyler <brown.tyler.w@gmail.com>
2023-08-15 19:28:10 -0700

    Added tentative city name change system

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import { getWeather } from './weather'
+import { getWeather } from './weather';
 import { searchCities } from './city';
 
 // Retrieves desired API information on page load
@@ -10,12 +10,24 @@ getWeather(10, 10, Intl.DateTimeFormat().resolvedOptions().timeZone).then(
   }
 )
 
+let search_button = document.getElementById('search-button');
+
 // Listens for updates on the search bar and calls API to search for cities that match string
+let city = "";
+let state = "";
+let location = "";
 let search_bar = document.getElementById('search-city');
 search_bar.addEventListener('change', (event) => {
   searchCities(search_bar.value).then(
     res => {
       console.log(res.data);
+      // Tentative name change based on first search result
+      // Locations data are stored under (res.data.results[0-9])
+      // Need to pull lat/lon data as well as well as country data to display country instead of state
+      city = res.data.results[0].name;
+      state = res.data.results[0].admin1;
+      location = city + ", " + state;
+      document.getElementById('city').innerHTML = location;
     }
   )
 })
