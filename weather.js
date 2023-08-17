0cbce01595f34efcf2162fd0fb46a0ba78080ef2
Tyler Brown <brown.tyler.w@gmail.com>
2023-08-16 23:06:55 -0700

    Added rain % chance in daily API call

--- weather.js
+++ weather.js
@@ -1,7 +1,7 @@
 // API call to retrieve desired weather data
 // Nothing more should need to be done here
 export function getWeather(lat, lon, timezone) {
-    return axios.get('https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=temperature_2m,precipitation_probability,precipitation,weathercode,cloudcover,windspeed_10m&current_weather=true&temperature_unit=fahrenheit&daily=weathercode,temperature_2m_max,temperature_2m_min,sunrise,sunset,precipitation_sum&timezone=America%2FLos_Angeles', {
+    return axios.get('https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=temperature_2m,precipitation_probability,precipitation,weathercode,cloudcover,windspeed_10m&current_weather=true&temperature_unit=fahrenheit&daily=weathercode,precipitation_probability_max,temperature_2m_max,temperature_2m_min,sunrise,sunset,precipitation_sum&timezone=America%2FLos_Angeles', {
         params: {
             latitude: lat,
             longitude: lon,
