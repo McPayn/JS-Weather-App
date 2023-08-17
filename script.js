97e74c29bbc56e534e12c6647a60d495388aa9ff
mcpayn <107960406+mcpayn@users.noreply.github.com>
2023-08-16 18:59:43 -0700

    Delete script.js
    File is not needed

--- script.js
+++
@@ -1,5 +0,0 @@
-// Changes displayed city name to whatever value is in the search bar when search button is clicked
-function changeCity(city) {
-    document.getElementById("city").innerHTML = city;
-  }
-
