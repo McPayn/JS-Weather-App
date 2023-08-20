c2657023b37b613a854ad5318dc7d2114a7b1e43
Tyler Brown <brown.tyler.w@gmail.com>
2023-08-20 10:29:12 -0700

    Code refactor and style change Refactored repeated code in main.js to separate function, changed font color to white

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Weather Web App
 A web application built using HTML, CSS, and JavaScript
-![Weather App Screenshot](https://github.com/McPayn/JS-Weather-App/assets/107960406/ad4fdff8-805d-4758-a19b-1fe5eb622b38)
+![Weather App Screenshot](https://github.com/McPayn/JS-Weather-App/assets/107960406/a8fb7ee2-7f98-435a-9a6a-1d91adf70c93)
 
 ## How It Works
 Using [open-meteo's](https://open-meteo.com) Weather Forecast and Geocoding APIs, this web app will allow you to search for
--- main.js
+++ main.js
@@ -6,33 +6,12 @@ let city, state, location = "";
 navigator.geolocation.getCurrentPosition(positionSuccess, positionError)
 
 // On page load, after allowing location to be shared, gets user's current location's weather data
-// TODO: Retrieve location name from returned coordinates to display in app
 function positionSuccess({ coords }) {
   getWeather(coords.latitude, coords.longitude, Intl.DateTimeFormat().resolvedOptions().timeZone).then(
     res => {
       console.log(res);
       // Get current/high/low temp information for location and displays it on screen
-      document.getElementById('temperature').innerHTML = res.current.currentTemp + '&deg F';
-      document.getElementById('high_temp').innerHTML = res.current.highTemp + '&deg F';
-      document.getElementById('low_temp').innerHTML = res.current.lowTemp + '&deg F';
-      document.getElementById('rain_chance').innerHTML = res.daily[0].rainChance + '%';
-      if (parseInt(res.daily[0].rainChance) >= 50) {
-        document.getElementById("day0icon").src="./img/rain-icon.png";
-      } else {
-        document.getElementById("day0icon").src="./img/sun-icon.png";
-      }
-      // Loops through and writes the 7-day forecast information to screen
-      for (let i = 1; i < 7; i++ ) {
-        document.getElementById('day' + i).innerHTML = res.daily[i].timestamp.slice(5);
-        document.getElementById('high_temp' + i).innerHTML = res.daily[i].maxTemp + '&deg F';
-        document.getElementById('low_temp' + i).innerHTML = res.daily[i].minTemp + '&deg F';
-        document.getElementById('rain_chance' + i).innerHTML = res.daily[i].rainChance + '%';
-        if (parseInt(res.daily[i].rainChance) >= 50) {
-          document.getElementById("day" + i + "icon").src="./img/rain-icon.png";
-        } else {
-          document.getElementById("day" + i + "icon").src="./img/sun-icon.png";
-        }
-      }
+      renderWeather(res);
       document.getElementById('city').innerHTML = 'Current Location';
     }
   )
@@ -50,7 +29,6 @@ search_bar.addEventListener('keyup', (event) => {
       console.log(res.data);
       // Tentative name change based on first search result
       // Locations data are stored under (res.data.results[0-9])
-      // Need to pull lat/lon data for weather
       city = res.data.results[0].name;
       // If city is not in the US, display country instead of state
       if (res.data.results[0].country_code === 'US') {
@@ -65,34 +43,38 @@ search_bar.addEventListener('keyup', (event) => {
       document.getElementById('city').innerHTML = location;
       // Automatically updates temperature information as it's typed in
       // Component is working, but needs to be restructured to update weather info when desired city is chosen from list
-      
       getWeather(lat, lon, timezone).then(
         res => {
           console.log(res);
           // Get current/high/low temp information for location and displays it on screen
-          document.getElementById('temperature').innerHTML = res.current.currentTemp + '&deg F';
-          document.getElementById('high_temp').innerHTML = res.current.highTemp + '&deg F';
-          document.getElementById('low_temp').innerHTML = res.current.lowTemp + '&deg F';
-          document.getElementById('rain_chance').innerHTML = res.daily[0].rainChance + '%';
-          if (parseInt(res.daily[0].rainChance) >= 50) {
-            document.getElementById("day0icon").src="./img/rain-icon.png";
-          } else {
-            document.getElementById("day0icon").src="./img/sun-icon.png";
-          }
-          // Loops through and writes the 7-day forecast information to screen
-          for (let i = 1; i < 7; i++ ) {
-            document.getElementById('day' + i).innerHTML = res.daily[i].timestamp.slice(5);
-            document.getElementById('high_temp' + i).innerHTML = res.daily[i].maxTemp + '&deg F';
-            document.getElementById('low_temp' + i).innerHTML = res.daily[i].minTemp + '&deg F';
-            document.getElementById('rain_chance' + i).innerHTML = res.daily[i].rainChance + '%';
-            if (parseInt(res.daily[i].rainChance) >= 50) {
-              document.getElementById("day" + i + "icon").src="./img/rain-icon.png";
-            } else {
-              document.getElementById("day" + i + "icon").src="./img/sun-icon.png";
-            }
-          } 
+          renderWeather(res);
         }
       )
     }
   )
 })
+
+// Renders weather data to screen
+function renderWeather(data) {
+  document.getElementById('temperature').innerHTML = data.current.currentTemp + '&deg F';
+  document.getElementById('high_temp').innerHTML = data.current.highTemp + '&deg F';
+  document.getElementById('low_temp').innerHTML = data.current.lowTemp + '&deg F';
+  document.getElementById('rain_chance').innerHTML = data.daily[0].rainChance + '%';
+  if (parseInt(data.daily[0].rainChance) >= 50) {
+    document.getElementById("day0icon").src="./img/rain-icon.png";
+  } else {
+    document.getElementById("day0icon").src="./img/sun-icon.png";
+  }
+  // Loops through and writes the 7-day forecast information to screen
+  for (let i = 1; i < 7; i++ ) {
+    document.getElementById('day' + i).innerHTML = data.daily[i].timestamp.slice(5);
+    document.getElementById('high_temp' + i).innerHTML = data.daily[i].maxTemp + '&deg F';
+    document.getElementById('low_temp' + i).innerHTML = data.daily[i].minTemp + '&deg F';
+    document.getElementById('rain_chance' + i).innerHTML = data.daily[i].rainChance + '%';
+    if (parseInt(data.daily[i].rainChance) >= 50) {
+      document.getElementById("day" + i + "icon").src="./img/rain-icon.png";
+    } else {
+      document.getElementById("day" + i + "icon").src="./img/sun-icon.png";
+    }
+  } 
+}
